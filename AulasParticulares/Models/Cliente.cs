061bbe9d9840682eba529594905bd0f537d601be
Lucas Pickler <lucas-pickler@hotmail.com>
2023-01-12 16:02:45 -0300

    Criando Modelo Cliente

---
+++ AulasParticulares/Models/Cliente.cs
@@ -0,0 +1,9 @@
+﻿namespace AulasParticulares.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public decimal Altura { get; set; }
+    }
+}
--- AulasParticulares/Program.cs
+++ AulasParticulares/Program.cs
@@ -1,42 +1,8 @@
-﻿string nome = CadastrarNome();
-int idade = CadastrarIdade();
-decimal altura = CadastrarAltura();
+﻿using AulasParticulares.Models;
 
-ApresentarDados();
+string nome = ProcessoDeCadastramento.CadastrarNome();
+int idade = ProcessoDeCadastramento.CadastrarIdade();
+decimal altura = ProcessoDeCadastramento.CadastrarAltura();
 
-decimal CadastrarAltura()
-{
-    Console.WriteLine("Qual a sua altura?");
-    decimal altura = Convert.ToDecimal(Console.ReadLine());
-    PularDuasLinhas();
-    return altura;
-}
+ProcessoDeCadastramento.ApresentarDados(nome, idade, altura);
 
-string CadastrarNome()
-{
-    Console.WriteLine("Qual o seu nome?");
-    string nome = Console.ReadLine();
-    PularDuasLinhas();
-
-    return nome;
-}
-
-int CadastrarIdade()
-{
-    Console.WriteLine("Qual a sua idade?");
-    int idade = Convert.ToInt32(Console.ReadLine());
-    PularDuasLinhas();
-
-    return idade;
-}
-
-void ApresentarDados()
-{
-    Console.WriteLine($"nome: {nome} \nidade: {idade} \naltura: {altura}");
-}
-
-void PularDuasLinhas()
-{
-    Console.WriteLine("");
-    Console.WriteLine("");
-}
