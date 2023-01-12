df3aaa674580a0bbf4ebb97cbedf72d6caeb93b5
Luiz Goebel <goebel.adm@gmail.com>
2023-01-12 16:09:14 -0300

    resolução

--- AulasParticulares/Program.cs
+++ AulasParticulares/Program.cs
@@ -4,18 +4,4 @@ string nome = ProcessoDeCadastramento.CadastrarNome();
 int idade = ProcessoDeCadastramento.CadastrarIdade();
 decimal altura = ProcessoDeCadastramento.CadastrarAltura();
 
-decimal CadastrarAltura()
-{
-    Console.WriteLine("Qual a sua altura?");
-    decimal altura = Convert.ToDecimal(Console.ReadLine());
-    PularDuasLinhas();
-    return altura;
-}
-
-string CadastrarNome()
-{
-    Console.WriteLine("Qual o seu nome?");
-    string nome = Console.ReadLine();
-    PularDuasLinhas();
-
-
+ProcessoDeCadastramento.ApresentarDados(nome, idade, altura);
