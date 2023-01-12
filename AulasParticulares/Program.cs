b665140362e050875475501cc0d0cedf8783f0f9
Lucas Pickler <lucas-pickler@hotmail.com>
2023-01-12 16:07:19 -0300

    teste

--- AulasParticulares/Program.cs
+++ AulasParticulares/Program.cs
@@ -3,7 +3,8 @@
 string nome = ProcessoDeCadastramento.CadastrarNome();
 int idade = ProcessoDeCadastramento.CadastrarIdade();
 decimal altura = ProcessoDeCadastramento.CadastrarAltura();
-
+ 
 ProcessoDeCadastramento.ApresentarDados(nome, idade, altura);
 
 
+ 
