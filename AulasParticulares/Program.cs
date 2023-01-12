17e9d8f34246407c821f376d90366c0034fbb32e
lucas <lucas@DESKTOP-CBDO4G8>
2023-01-12 14:24:01 -0300

    Adicionar arquivos de projeto.

---
+++ AulasParticulares/Program.cs
@@ -0,0 +1,40 @@
+﻿string nome = CadastrarNome();
+int idade = CadastrarIdade();
+decimal altura = CadastrarAltura();
+
+ApresentarDados();
+
+decimal CadastrarAltura()
+{
+    Console.WriteLine("Qual a sua altura?");
+    decimal altura = Convert.ToDecimal(Console.ReadLine());
+    PularDuasLinhas();
+    return altura;
+}
+
+string CadastrarNome()
+{
+    Console.WriteLine("Qual o seu nome?");
+    string nome = Console.ReadLine();
+    PularDuasLinhas();
+    return nome;
+}
+
+int CadastrarIdade()
+{
+    Console.WriteLine("Qual a sua idade?");
+    int idade = Convert.ToInt32(Console.ReadLine());
+    PularDuasLinhas();
+    return idade;
+}
+
+void ApresentarDados()
+{
+    Console.WriteLine($"nome: {nome} \nidade: {idade} \naltura: {altura}");
+}
+
+void PularDuasLinhas()
+{
+    Console.WriteLine("");
+    Console.WriteLine("");
+}
